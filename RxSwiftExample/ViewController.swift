7423d932f084eef82360a2a560586e045b912cda
Renato Mateus <renatomateusx@gmail.com>
2021-02-28 15:28:30 -0300

    Initialize Commit

--- RxSwiftExample/ViewController.swift
+++ RxSwiftExample/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxRelay
 
 class ViewController: UIViewController {
-
+    
+    private let loginViewModel = LoginViewModel()
+    private let disposeBag = DisposeBag()
+    
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var btnlogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureFields()
+    }
+    
+    func configureFields()
+    {
+        loginTextField.becomeFirstResponder()
+       
+        loginTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.loginPublishSubject).disposed(by: disposeBag)
+        passwordTextField.rx.text.map { $0 ?? "" }.bind(to: loginViewModel.passwordPublishSubject).disposed(by: disposeBag)
+        
+        loginViewModel.isValid().bind(to: btnlogin.rx.isEnabled).disposed(by: disposeBag)
+        loginViewModel.isValid().map { $0 ? 1 : 0.1 }.bind(to: btnlogin.rx.alpha).disposed(by: disposeBag)
     }
 
-
+    @IBAction func btnLoginAction(_ sender: UIButton) {
+        print("Login Button tapped")
+    }
+    
 }
+
+class LoginViewModel {
+    let loginPublishSubject = PublishSubject<String>()
+    let passwordPublishSubject = PublishSubject<String>()
+    
+    func isValid() -> Observable<Bool> {
+        return Observable.combineLatest(loginPublishSubject.asObservable().startWith(""), passwordPublishSubject.asObservable().startWith("")).map { username, password in
+            return username.count > 3 && password.count > 3
+        }.startWith(false)
+    }
+}
+
 
